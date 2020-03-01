820a671afad8894b3445c675c108c4c890930560
Serhey Verameichyk <babavera81@gmail.com>
2020-03-01 17:08:52 +0300

    упростил и поправил

--- Pr10/src/by/verameichyk/Main.java
+++ Pr10/src/by/verameichyk/Main.java
@@ -3,17 +3,13 @@ package by.verameichyk;
 public class Main {
 
     public static void main(String[] args) {
-	    int i = 1789325412;
-	    int sum = 0;
-	    int n = 1;
-	    int x = 0;
-        int y = 0;
-	    while (x != i) {
-            x = i % n;
-            y = (x * 10) / n;
-            n = n * 10;
-            sum += y;
+        int i = 1234567890;
+        System.out.print("Сумма цифр числа " + i);
+        int sum = 0;
+        while (i != 0) {
+            sum += i % 10;
+            i /= 10;
         }
-        System.out.println("Сумма цифр числа " + i + " равна " + sum);
+        System.out.println(" равна " + sum);
     }
 }
