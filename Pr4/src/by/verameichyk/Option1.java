9c2840ff0b3ce539c87e5717d987cebdd360073e
Serhey Verameichyk <babavera81@gmail.com>
2020-03-06 18:08:21 +0300

    third commit

--- Pr4/src/by/verameichyk/Option1.java
+++ Pr4/src/by/verameichyk/Option1.java
@@ -2,14 +2,15 @@ package by.verameichyk;
 
 public class Option1 {
     public static void main(String[] args) {
-        double a, b, r, d;
+        double a, b, d, r;
         a = 10;
         b = 20;
-        d = 22.4;
+        r = 11.18034;
+        d = 2 * r;
         if ((d * d) >= (a * a) + (b * b)) {
-            System.out.println("Круг радиусом " + (d / 2) + " закроет прямоугольик со сторонами a = " + a + " и b = " + b);
+            System.out.println("Круг радиусом " + r + " закроет прямоугольик со сторонами a = " + a + " и b = " + b);
         } else {
-            System.out.println("Круг радиусом " + (d / 2) + " не закроет прямоугольик со сторонами a = " + a + " и b = " + b);
+            System.out.println("Круг радиусом " + r + " не закроет прямоугольик со сторонами a = " + a + " и b = " + b);
         }
     }
 }
